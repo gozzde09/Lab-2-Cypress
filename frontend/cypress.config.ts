3784410f9361d6113964a561708c1fa042abc2d8
Gzd Akgn <gozzde0990@gmail.com>
2024-12-03 13:46:23 +0100

    compose

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "cypress";
 import codeCoverage from "@cypress/code-coverage/task";
+import babelrc from "@cypress/code-coverage/use-babelrc";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       codeCoverage(on, config);
+      on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"));
 
       // Return the config object
       return config;
